cce87cfcb2530ff34912355c4f904264b8271d43
Jose <142075563+BirdRoad1@users.noreply.github.com>
2025-07-28 21:09:37 -0400

    Upload files

---
+++ README.md
@@ -0,0 +1,18 @@
+# CatPapers
+
+A python script for Linux that changes your wallpaper to a random cat wallpaper from Reddit.
+
+## How to use
+
+In a terminal, run `python3 catpapers.py` or `./catpapers.py`.
+
+You can also use this in a bash script or a cron script as shown in the next section.
+
+## Cron script
+To make the wallpaper change periodically, you can use cron to execute this script periodically.
+
+The following command appends a line to your user crontab that runs catpapers.py every 5 minutes
+
+```bash
+(crontab -l 2>/dev/null; echo "*/5 * * * * python3 /path/to/catpapers.py") | crontab -
+```
---
+++ catpapers.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+from urllib.request import urlopen, Request
+import json
+import os.path as path
+import os
+import subprocess
+import random
+
+def get_reddit_posts():
+    req = Request('https://www.reddit.com/r/cats.json?limit=100', headers={
+        'user-agent': 'catpapers/1.0'
+    })
+    res = urlopen(req)
+    data = json.loads(res.read())
+    return data['data']['children']
+
+def download_file(url: str, dest: str):
+    req = Request(url, headers={
+        'user-agent': 'catpapers/1.0'
+    })
+
+    res = urlopen(req)
+    with open(dest, 'wb') as file:
+        file.write(res.read())
+
+def apply_wallpaper(path: str) -> bool:
+    print(f'Loaded wallpaper: {path}')
+    return subprocess.call(['/usr/bin/feh','--bg-scale',path]) == 0
+
+def main():
+    imagesDir = path.abspath('./images/')
+
+    try:
+        posts = get_reddit_posts()
+    except Exception as err:
+        print(f'Failed to get reddit posts: {err}')
+        return
+
+    found_cat = False
+    while len(posts) > 0:
+        post = random.choice(posts)
+        posts.remove(post)
+        post_data = post['data']
+        if post_data['is_video']:
+            continue # skip videos
+        url: str = post_data['url']
+        if not url.endswith('png') and not url.endswith('jpg') and not url.endswith('.jpeg'):
+            continue # only png/jpeg
+        
+        file_name = url.split('/').pop().replace('/','')
+
+        imagePath = path.join(imagesDir, file_name)
+        if path.exists(imagePath):
+            print('Image already exists! Skipping...')
+            continue
+
+        found_cat = True
+        break
+
+    if not found_cat:
+        print('No new cats found! Using existing cat!')
+        imgs = os.listdir(imagesDir)
+        if len(imgs) == 0:
+            print('No cat images stored :(')
+            exit(1)
+        
+        file = path.join(imagesDir, random.choice(imgs))
+        apply_wallpaper(file)
+    else:
+        if not path.exists(imagesDir):
+            os.makedirs(imagesDir)
+
+        # download image
+        try:
+            download_file(url, imagePath)
+        except Exception as err:
+            print(f'Failed to download image! {err}')
+            return
+
+        if not path.exists(imagePath):
+            print('Downloaded image does not exist')
+            return
+        
+        status = apply_wallpaper(imagePath)
+        print('Status: ' + str(status))
+
+if __name__ == '__main__':
+    main()
